ce7946b19bdfc42feb763128d1aee1cf4dcf2d39
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-12-05 23:19:59 +0530

    new

--- script.js
+++ script.js
@@ -34,26 +34,18 @@ document.querySelector('.check').addEventListener('click', function () {
         }
         
     } 
-    
-    else if (guess > secretNumber) {
+    else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Too High!';
+            document.querySelector('.message').textContent = guess >secretNumber ?'Too High!':'Too Low!';
             score--;
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.message').textContent = 'You Lost!';
         }
     } 
-    
-    else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too Low!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
+    else {
             document.querySelector('.message').textContent = 'You Lost!';
         }
-    }
 });
 
 document.querySelector('.again').addEventListener('click', function() {
