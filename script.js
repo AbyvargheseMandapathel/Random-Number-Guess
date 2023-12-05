d8a06bd88829c0732c4e9d17739042108621848b
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-12-05 23:27:13 +0530

    refactored

--- script.js
+++ script.js
@@ -14,16 +14,20 @@ let score = 20;
 let highscore = 0;
 // document.querySelector('.number').textContent = secretNumber;
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
     if (!guess) {
-        document.querySelector('.message').textContent = 'Input Number!';
+        displayMessage("No Number")
     } 
     
     else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct Number!';
+        displayMessage("Correct Number!")
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor= '#60b347';
         document.querySelector('.number').style.width = '30rem';
@@ -36,7 +40,8 @@ document.querySelector('.check').addEventListener('click', function () {
     } 
     else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = guess >secretNumber ?'Too High!':'Too Low!';
+            // document.querySelector('.message').textContent = guess >secretNumber ?'Too High!':'Too Low!';
+            displayMessage(guess >secretNumber ?'Too High!':'Too Low!')
             score--;
             document.querySelector('.score').textContent = score;
         } else {
@@ -53,7 +58,7 @@ document.querySelector('.again').addEventListener('click', function() {
     
     score = 20;
     document.querySelector('.score').textContent = score;
-    document.querySelector('.message').textContent= 'Start guessing...';
+    displayMessage("Start guessing...")
     document.querySelector('.number').textContent = "?";
     document.querySelector('body').style.backgroundColor= '#222';
     document.querySelector('.number').style.width = '15rem';
